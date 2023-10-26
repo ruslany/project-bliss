26cd70e30c3902c0549301ea251aa1477a81df44
Ruslan Yakushev <1664475+ruslany@users.noreply.github.com>
2023-10-26 10:17:56 -0700

    fafdsa

--- src/Bliss/Classes/DagNodeExecutor.cs
+++ src/Bliss/Classes/DagNodeExecutor.cs
@@ -6,7 +6,7 @@ public class DagNodeExecutor : IDagNodeExecutor
 {
     public async Task<int> ExecuteAsync(IDagNode unitOfExecution)
     {
-        unitOfExecution.ExecuteAsync();
+        await unitOfExecution.ExecuteAsync();
         await Task.Delay(1000); // Replace this with your actual implementation
         return 0; // Replace this with your actual implementation
     }
